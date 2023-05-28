0683a3ae91be17ea4369e1a515eb4bc790b8b6c0
Simon Hoefling <simon@hoeflia.eu>
2023-05-28 23:14:29 +0800

    Added validations in the note model

--- app/models/note.rb
+++ app/models/note.rb
@@ -1,2 +1,6 @@
 class Note < ApplicationRecord
+  # This is a class model where all the methods are stored for the notes.
+  # The methods are called in the controller to perform the actions.
+  validates :name, presence: true
+  validates :description, presence: true
 end
